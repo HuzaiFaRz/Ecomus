815f17b707b3fed9edc22fc8935ae451976bd9bc
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-29 05:20:48 -0700

    Some

--- script.js
+++ script.js
@@ -1,17 +1,107 @@
-// var mySwiper = new Swiper(".swiper-container", {
-//   // Optional parameters
-//   direction: "horizontal",
-//   loop: true,
-//   effect: "slide",
+const header = document.querySelector(".header");
+const headerCloseBtn = document.querySelector(".top-header-close-btn");
 
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
+function headerTopSwipperSlider() {
+  var mySwiper = new Swiper(".top-header-swipper-container", {
+    direction: "horizontal",
+    loop: true,
+    effect: "slide",
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".next",
-//     prevEl: ".prev",
-//   },
-// });
+    autoplay: {
+      delay: 3000,
+    },
+
+    navigation: {
+      nextEl: ".top-header-swipper-next",
+      prevEl: ".top-header-swipper-prev",
+    },
+  });
+  if (headerCloseBtn) {
+    headerCloseBtn.addEventListener("click", () => {
+      header.style.transform = "translateY(-100%)";
+    });
+  }
+}
+headerTopSwipperSlider();
+
+// <!-- <!DOCTYPE html>
+// <html>
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+// <link
+//   href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
+//   rel="stylesheet"
+// />
+
+// <link
+//   rel="stylesheet"
+//   href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+// />
+//     <link rel="stylesheet" href="index.css" />
+//   </head>
+//   <body>
+//     <main class="bg-slate-700 w-full h-full cursor-pointer select-none overflo">
+//       <header
+//         class="bg-[#c38361] w-full h-10 flex flex-row justify-center items-center"
+//       >
+// <div
+//   class="swiper-container h-full flex flex-row justify-center items-center"
+// >
+//   <div
+//     class="swiper-wrapper h-full flex flex-row justify-center items-center"
+//   >
+//     <div
+//       class="swiper-slide w-6/12 h-full flex flex-col justify-center items-center text-center text-white font-light tracking-wider text-md font-['Albert_Sans\AlbertSans-Black.ttf']"
+//     >
+//       Fashion Frenzy: Up to 60% off on all styles
+//     </div>
+//     <div
+//       class="swiper-slide w-6/12 h-full flex flex-col justify-center items-center text-center text-white font-bold"
+//     >
+//       Welcome to our store
+//     </div>
+//   </div>
+
+//   <div class="swiper-pagination"></div>
+
+//   <i class="next ri-arrow-right-s-line"></i>
+//   <i class="prev ri-arrow-left-s-line"></i>
+// </div>
+//          <i class="ri-close-line self-end"></i>
+//       </header>
+//       <nav class="w-full flex items-center justify-between px-7 h-24">
+//         <img src="Logo/Logo.svg" alt="Logo" class="nav-log" id="NavLogo" />
+//         <div class="nav-links-dropdpowns flex">
+//           <div class="nav-link-dropdown relative overflow-hidden">
+//             <div
+//               class="nav-link-dropdown-btn text-base font-normal tracking-normal text-white flex"
+//             >
+//               Home<i class="ri-arrow-down-s-line text-lg font-light"></i>
+//             </div>
+//             <ul
+//               class="nav-dropdown-menu flex flex-col justify-center items-center gap-8 absolute top-full left-1/2 -translate-y-full -translate-x-2/4 w-screen h-screen bg-black"
+//             >
+//               <div class="nav-dropdown-menu-part-1 flex w-full bg-black">
+//                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//                 Temporibus reiciendis consequuntur dicta dolor officia est,
+//                 beatae sunt ducimus quasi atque eaque, blanditiis a accusamus
+//                 asperiores rem ipsam quas similique maiores.
+//               </div>
+//               <div class="nav-dropdown-menu-part-1 flex flex w-full bg-black">
+//                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//                 Temporibus reiciendis consequuntur dicta dolor officia est,
+//                 beatae sunt ducimus quasi atque eaque, blanditiis a accusamus
+//                 asperiores rem ipsam quas similique maiores.
+//               </div>
+//             </ul>
+//           </div>
+//         </div>
+//         <div class="nav-icons"></div>
+//       </nav>
+//     </main>
+//     <script src="https://cdn.tailwindcss.com"></script>
+//     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+//     <script src="app.js"></script>
+//   </body>
+// </html> -->
