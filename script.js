4d277cdf846f8637561a85a22b63a32346bd4106
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-06-29 04:20:32 -0700

    Some Editing

---
+++ script.js
@@ -0,0 +1,17 @@
+// var mySwiper = new Swiper(".swiper-container", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
+//   effect: "slide",
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".next",
+//     prevEl: ".prev",
+//   },
+// });
